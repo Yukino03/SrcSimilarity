cf4be08cf5baf88225843021a09376797de7ae5f
Yukino03 <16j5003@stu.meisei-u.ac.jp>
2019-04-02 15:30:41 +0900

    hogehogeeeee

--- src/main.py
+++ src/main.py
@@ -5,18 +5,22 @@ from mylib import src
 from mylib import diff
 
 src1 = src.SrcList(__file__)
+src2 = src.SrcList(__file__)
 # for x in range(len(src1)):
   # print(src1.tokenize(x))
 
 # wordを数える
-dic = {}
-for x in range(len(src1)):
-  line = src1.tokenize(x)
-  for token in line:
-    if token in dic:
-      dic[token] = dic[token]+1
-    else :
-      dic[token] = 1
-dic_sorted = sorted(dic.items(), key=lambda x:x[1], reverse = True)
-for word in dic_sorted:
-  print(word)
+# dic = {}
+# for x in range(len(src1)):
+#   line = src1.tokenize(x)
+#   for token in line:
+#     if token in dic:
+#       dic[token] = dic[token]+1
+#     else :
+#       dic[token] = 1
+# dic = diff.extract_alph(dic)
+# dic_sorted = sorted(dic.items(), key=lambda x:x[1], reverse = True)
+# for word in dic_sorted:
+#   print(word)
+
+diff.cmp_src(src1, src2)
--- src/mylib/diff.py
+++ src/mylib/diff.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import re
-from tokenize import tokenize
-from io import BytesIO
-# compare srcs
+
 def cmp_src(src1, src2):
-  print("stab")
+  '''read tokens sequently'''
+  dic1 = word_count(src1)
+  dic2 = word_count(src2)
+  dic1 = extract_alph(dic1)
+  dic2 = extract_alph(dic2)
+  list1 = sort_dic(dic1)
+  list2 = sort_dic(dic2)
+  print("DEBUG","list1")
+  for x in list1:
+    print(x)
+  print("DEBUG","list2")
+  for y in list2:
+    print(y)
 
-# compare lines
-def cmp_line(line1, line2):
-  return line1 == line2
+def extract_alph(dic):
+  '''Extract alphabet from token dict'''
+  alp_dic = {}
+  for key in dic.keys():
+    if isname(key):
+      alp_dic[key] = dic[key]
+  return alp_dic
 
-# func() -> func
-def extract_func(line):
-# .*(?=\(.*\))
-# 対応した括弧より前を抽出
-  result = re.compile(".*(?=\(.*\))").match(line)
-# 後方から見て最初の空白orカンマまでを抽出
-# result = re.compile("").match(line)
+def word_count(src1):
+  dic = {}
+  for x in range(len(src1)):
+    line = src1.tokenize(x)
+    for token in line:
+      if token in dic:
+        dic[token] = dic[token]+1
+      else :
+        dic[token] = 1
+  return dic
 
-  return result
+def sort_dic(dic):
+  dic_sorted = sorted(dic.items(), key=lambda x:x[1], reverse = True)
+  return dic_sorted
+
+def isname(string):
+  '''コメントや記号類を弾く'''
+  if str.isalpha(string[0]):
+    return True
+  elif str.isdigit(string[0]):
+    return False
